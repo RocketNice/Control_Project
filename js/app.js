8203610167181c261ffed7b201281867868d0221
RocketNice <lisdb@bk.ru>
2022-07-15 14:00:42 +0300

    js_rename

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const background_rename = document.getElementById('bil')
 const background_rename1 = document.getElementById('billi')
 const background_rename2 = document.getElementById('font')
+const background_rename3 = document.getElementById('zero')
+const background_rename4 = document.getElementById('middle')
+const background_rename5 = document.getElementById('end')
+
 bil.onclick = () => {
     setTimeout(() => {
         bil.style.color = '#FFFFFF'
@@ -10,12 +14,20 @@ bil.onclick = () => {
     font.style.animationDuration = '1s'
     font.style.animationFillMode = 'forwards'
     font.style.animationIterationCount = '1'
+    zero.textContent = '4'
+    middle.textContent = '3'
+    end.textContent = '30'
 }
 billi.onclick = () => {
     bil.style.color = 'black'
-    billi.style.color = '#FFFFFF'
+    setTimeout(() => {
+        billi.style.color = '#FFFFFF'
+    }, 800)
     font.style.animationName = 'reverse_block'
     font.style.animationDuration = '1s'
     font.style.animationFillMode = 'forwards'
     font.style.animationIterationCount = '1'
+    zero.textContent = '0'
+    middle.textContent = '8'
+    end.textContent = '16'
 }
